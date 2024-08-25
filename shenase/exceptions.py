d5cf17d35f6959fe6e1c07e2ff9b38b5af0bd089
Artin Mohammadi <sheikhartin@gmail.com>
2024-08-25 06:59:59 +0330

    Exceptions that could not be here

---
+++ shenase/exceptions.py
@@ -0,0 +1,59 @@
+from fastapi import HTTPException, status
+
+
+class UserNotFoundError(HTTPException):
+    def __init__(self, username: str) -> None:
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with username `{username}` not found.',
+        )
+
+
+class InactiveUserError(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Inactive user.',
+        )
+
+
+class UsernameAlreadyExistsError(HTTPException):
+    def __init__(self, username: str) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Username `{username}` already registered.',
+        )
+
+
+class EmailAlreadyExistsError(HTTPException):
+    def __init__(self, email: str) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Email `{email}` already registered.',
+        )
+
+
+class UserCreationError(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Failed to create user.',
+        )
+
+
+class IncorrectUsernameOrPasswordError(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect username or password.',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+
+
+class CredentialsError(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate credentials.',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
